d19bc57c9c99ce7404ba926b0be91e160f3575e7
dlcruz13777 <78450877+dlcruz13777@user.noreply.github.com>
2022-07-04 14:46:20 -0700

    progress

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-// npm - global command, comes with node
-// npm --version
+const { readFile, writeFile } = require('fs').promises;
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
-// local dependency = use it only in this particular project
-// npm i <packageName>
 
-// gloabal dependency - use it in any project
-// npm install -g <packageName>
-// sudo npm install -g <packageName> (mac)
+const start = async() => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf-8');
+        const second = await readFile('./content/second.txt', 'utf-8');
+        await writeFile(
+            './content/result-mind-grenade.txt', 
+            `THIS IS AWESOME : ${first} ${second}`,
+            { flag: 'a' }
+        );
+        console.log(first, second);
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json in the root, create properties, etc.)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
+start();
 
-const _ = require('lodash');
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf-8', (err, data) => {
+//             if (err){
+//                 reject(err);
+//             } else {
+//                 resolve(data);
+//             }
+//         });
+//     });
+// };
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
+// getText('./content/first.txt')
+//     .then(result => console.log(result))
+//     .catch(err => console.log(err));
 
-console.log(newItems);
