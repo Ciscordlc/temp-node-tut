778177828e0fa96e450f0b7d184a2183b8e20185
dlcruz13777 <78450877+dlcruz13777@user.noreply.github.com>
2022-07-19 11:23:22 -0700

    Created controllers folder

--- 2-express-tutorial/routes/auth.js
+++ 2-express-tutorial/routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 // The path matches the action in the form tag in the index.html file within methods-public folder ('/login')
 router.post('/', (req, res) => {
-    const {name} = req.body
+    const { name } = req.body
     if(name){
         return res.status(200).send(`Welcome ${name}`)
     }
